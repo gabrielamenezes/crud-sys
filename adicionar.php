8d8b2530cfc4763ee5f67436a090f77eda1d840a
Gabriela Menezes <gabimenezess10@gmail.com>
2021-02-11 16:17:56 -0300

    form de add user

---
+++ adicionar.php
@@ -0,0 +1,25 @@
+<h1>ADICIONAR USUÁRIO</h1>
+<form action="adicionar_action.php" method="post">
+    <label for="">
+        Nome:<br>
+        <input type="text" name="name">
+    </label><br><br>
+
+    <label for="">
+        Nascimento:<br>
+        <input type="date" name="birth">
+    </label><br><br>
+
+    <label for="">
+        Sexo:<br>
+        <input type="text" name="sex">
+    </label><br><br>
+
+    <label for="">
+        UF:<br>
+        <input type="text" name="estado">
+    </label><br><br>
+
+    <input type="submit" value="Adicionar">
+
+</form>

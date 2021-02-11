d6c3520341488162916f421196ea20350bb3c2b1
Gabriela Menezes <gabimenezess10@gmail.com>
2021-02-11 16:02:58 -0300

    starting table

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-    
-
+    // R -> READ;
+    require('config.php');
 ?>
+
+<table border="1" width="100%">
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>NASCIMENTO</th>
+        <th>SEXO</th>
+        <th>UF</th>
+        <th>AÇÕES</th>
+    </tr>
+</table>

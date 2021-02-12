923ade14ecdce04ab30832c5a17f9f91cd116ce5
Gabriela Menezes <gabimenezess10@gmail.com>
2021-02-12 17:21:27 -0300

    starting Read

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
     // R -> READ;
     require('config.php');
+    $lista = [];
+    //peguei os usuarios
+    $sql = $pdo->query("SELECT * FROM usuarios");
+
+
+    //verificar se tem algum usuario
+    if($sql->rowCount() > 0) {
+        $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
 ?>
 
 <a href="adicionar.php">ADICIONAR USUÁRIO</a>
@@ -8,8 +17,19 @@
     <tr>
         <th>ID</th>
         <th>NOME</th>
-        <th>SEXO</th>
         <th>EMAIL</th>
         <th>AÇÕES</th>
     </tr>
+
+    <?php foreach($lista as $tupla): ?>
+        <tr>
+            <td><?=$tupla['id'];?></td>
+            <td><?=$tupla['nome'];?></td>
+            <td><?=$tupla['email'];?></td>
+            <td>
+                
+            </td>
+
+        </tr>
+    <?php endforeach; ?>
 </table>
